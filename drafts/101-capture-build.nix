f4edff34f86612c32371046b20d0f759609cd559
Amit Kumar Das <amitd2@vmware.com>
2023-04-01 13:57:57 +0530

    Update and rename capture-build.nix to 101-capture-build.nix

--- drafts/101-capture-build.nix
+++ drafts/101-capture-build.nix
@@ -1,4 +1,5 @@
 # refer: https://scrive.github.io/nix-workshop/04-derivations/05-standard-derivation.html
+# run: cat $(nix-build /nix/store/5rgcvwndbc4525ypbb0r1vgqpbxgcy2g-env.drv)
 
 nix-repl> nixpkgs.stdenv.mkDerivation {
             name = "env";
