a6519f05a1ad4e1a22c2574300a7b609f8b93791
Amit Kumar Das <amitd2@vmware.com>
2023-04-01 14:08:31 +0530

    Update 101-build-inputs.nix

--- drafts/101-build-inputs.nix
+++ drafts/101-build-inputs.nix
@@ -1,6 +1,9 @@
 # refer: https://scrive.github.io/nix-workshop/04-derivations/05-standard-derivation.html
 # run: nix-build /nix/store/in40c5fl13ziqzds3wfg2ag7ax2xmq5l-greet-alice.drv
 # run: cat /nix/store/kp32rzq63barqa55q3mf761gsggi2bq6-greet-alice
+# run: nix show-derivation $drv
+# run: cat /nix/store/9krlzvny65gdc8s7kpb6lkx8cd02c25b-default-builder.sh
+# run: nix-shell $drv --run 'cat $stdenv/setup'
 
 nix-repl> nixpkgs.stdenv.mkDerivation {
             name = "greet-alice";
