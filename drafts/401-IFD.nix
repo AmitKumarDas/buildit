8d1a2e31f6116807a50168ae3d7960be5e6493bb
Amit Kumar Das <amitd2@vmware.com>
2023-04-06 15:56:51 +0530

    Create 401-IFD.nix

---
+++ drafts/401-IFD.nix
@@ -0,0 +1,24 @@
+# refer: https://jade.fyi/blog/nix-evaluation-blocking/
+
+let
+  pkgs = import <nixpkgs> { };
+  wait = n: builtins.readFile (pkgs.runCommand "delay" { } ''
+    sleep ${toString n}
+
+    echo $((${toString n} * 5)) > $out
+  '');
+
+in
+pkgs.stdenv.mkDerivation {
+  name = "blah";
+  dontUnpack = true;
+  doInstall = false;
+  dontConfigure = true;
+  buildPhase = ''
+    echo ${wait 1}
+    echo ${wait 2}
+    echo ${wait 3}
+    touch $out
+  '';
+  doBuild = true;
+}
