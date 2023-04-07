4b2424688f855117a826afd8a2e362fca94f457e
Amit Kumar Das <amitd2@vmware.com>
2023-04-07 17:03:07 +0530

    Update 201-pinning.sh

--- build/nix/snippets/201-pinning.sh
+++ build/nix/snippets/201-pinning.sh
@@ -1,4 +1,11 @@
-run: nix-env -iA nixpkgs.niv
+# refer: https://dimitrije.website/posts/2023-03-04-nix-ocaml.html
+
+nix-env -iA nixpkgs.niv
+
 mkdir repo && cd repo
 git init
 niv init && niv update nixpkgs -b 22.11
+
+# niv will pin this to tip of main branch
+# https://github.com/dimitrijer/nixfiles
+niv add dimitrijer/nixfiles -b main
